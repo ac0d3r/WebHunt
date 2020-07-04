b5276a5ae73f432efdea668fdcef32c19b79f57d
whoami <admin@imipy.com>
2020-07-04 21:07:08 +0800

    feat: upload to pypi.org

--- README.md
+++ README.md
@@ -8,7 +8,21 @@ just for fun
 
 对 https://github.com/webanalyzer/rules 组件规则的实现，修改部分规则，新增了支持多线程，支持管理组件更新、同步等功能。
 
+## Installation
+
+```bash
+pip install webhunt
+```
+
 ## Usage
+
+- man
+
+```bash
+webhunt --help
+webhunt manage --help
+webhunt scan --help
+```
 
 - 从 [rules](https://github.com/webanalyzer/rules) 更新组件
 
--- setup.py
+++ setup.py
@@ -3,16 +3,30 @@ __author__ = '@buzz'
 
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="webhunt",
     version="0.1",
     description="A Web Component Recognition Tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="@buzz",
-    url="",
+    url="https://github.com/Buzz2d0/WebHunt",
     platforms=["all"],
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
+    ],
+    install_requires=[
+        'requests',
+        'pysocks',
+        'click',
+        'beautifulsoup4',
+        'html5lib',
+        'pymysql',
+        'pypinyin'
     ],
     entry_points={
         'console_scripts': [
